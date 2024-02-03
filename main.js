c158ffe5e1602fa80af2132dcfb05b0f00d8204b
shayanhasiri <shynhsiri@gmail.com>
2024-02-03 08:19:57 +0330

    text update

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ function myMenuFunction(){
 
 /* ----- TYPING EFFECT ----- */
  var typingEffect = new Typed(".typedText",{
-    strings : ["Developer","Back-end Developer","Programmer"],
+    strings : ["Developer","Backend Developer","Programmer"],
     loop : true,
     typeSpeed : 100, 
     backSpeed : 80,
