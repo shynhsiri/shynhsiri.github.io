128a370a421c2ca1f75d71a8b74f1e7b93b183ed
shynhsiri <shynhsiri@gmail.com>
2024-04-21 19:19:38 +0330

    new effect edit

--- main.js
+++ main.js
@@ -194,18 +194,3 @@ particlesJS("particles", {
   },
   retina_detect: true
 });
-
-function scrollToTop() {
-  window.scrollTo({
-      top: 0,
-  });
-}
-
-window.addEventListener('scroll', function () {
-  var scrollTopButton = document.querySelector('.scroll-top');
-  if (this.window.pageYOffset > 200) {
-      scrollTopButton.style.display = 'block';
-  } else {
-      scrollTopButton.style.display = 'none';
-  }
-});
